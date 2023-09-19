608e2bc367f49697ab8bcbd4ef94d3bedb4063ed
Harsh4766 <yadavhd04@gmail.com>
2023-09-19 15:43:36 +0530

    change

---
+++ MindWell/app/src/main/java/com/example/mindwell/splashscreen.java
@@ -0,0 +1,35 @@
+package com.example.mindwell;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class splashscreen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(splashscreen.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },5000);
+    }
+
+
+}
